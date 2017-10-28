c4e18b25ff92ced18c6ed31fc21f911094d45926
Vincent <chenjuzen@gmail.com>
2017-10-28 17:39:50 +0800

    add client 5GHz

--- runtest.py
+++ runtest.py
@@ -1,6 +1,6 @@
 import requests as rq
 
-res = rq.get('http://ostrich-beta.kaiwoo.ai/v2/organisations/260/devices/126/survey/packets?interface=5GHz&duration=30&output=text&verbosity=high')
-# res = rq.get('http://ostrich-beta.kaiwoo.ai/v2/organisations/260/devices/126/survey/wifi/probe_reqs?action=stop')
+# res = rq.get('http://ostrich-beta.kaiwoo.ai/v2/organisations/260/devices/126/survey/packets?interface=5GHz&duration=30&output=text&verbosity=high')
+res = rq.get('http://ostrich-beta.kaiwoo.ai/v2/organisations/260/devices/126/survey/wifi/probe_reqs?action=start&interface=5GHz')
 # res = rq.get('http://captive.portal.my/verify?res=notyet&uamip=192.168.1.1&uamport=9486&challenge=ae270583a162abde24f6c41663faf102&called=A4-34-D9-95-11-59&mac=F4-8C-50-BA-D5-A6&ip=192.168.1.119&nasid=nas01&sessionid=59f362ed00000005&userurl=http://www.winfarm.com')
 print(res)
